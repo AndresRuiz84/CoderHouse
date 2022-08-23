3e6c85f6701f1d149712247f23d6dea5cd3c70a5
Andres Ruiz <98923276+AndresRuiz84@users.noreply.github.com>
2022-08-22 21:00:41 -0300

    22/08 control de flujos

---
+++ Practica/controlDeFlujos.js
@@ -0,0 +1,12 @@
+/* CONDICIONALES */
+
+const name = 'Andres'
+const apellido = 'Ruiz'
+let edad = prompt('Ingresa tu edad')
+
+if (edad <= 38){
+    console.log('si tiene mas de 18!!')
+    alert('puedes consumir alcohol')
+}else{
+    console.log('Es un pibito menor de 18')
+}
---
+++ Practica/index.js
@@ -0,0 +1,34 @@
+
+let nombre = "Andres"
+let edad=5
+const anio = 1984
+
+let numA = 2
+let numB = 3
+const NUMC = 4
+
+let resSuma = numA + numB
+let resResta = numB - numA
+let resMulti = numA * NUMC
+
+//CONCATENACION
+
+// console.log(resSuma)
+
+let pal1= "Pedro"
+let pal2 = "Picapiedra"
+
+const FRASE = pal1+ " " + pal2
+
+// console.log(FRASE)
+
+//PROMPT y ALERT
+
+// let nombreIngrsar = prompt("Ingrese su nombre: ")
+// console.log(nombreIngrsar)
+
+// alert(nombreIngrsar)
+
+const claseCursada = prompt('Cuantas clases has cursado?')
+
+console.log(pal1, pal2, claseCursada, typeof claseCursada, FRASE)
