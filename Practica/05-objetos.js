924370d26278743a77a7eb73b379e52d405cf888
Andres Ruiz <98923276+AndresRuiz84@users.noreply.github.com>
2022-09-05 20:00:43 -0300

    05/09

---
+++ Practica/05-objetos.js
@@ -0,0 +1,15 @@
+
+const persona = {
+
+    nombre: 'Andres',
+    apellido: 'Ruiz',
+    edad: 38,
+    pais: 'Argentina',
+    vivo: true
+}
+
+console.log(persona)
+
+alert(persona)
+
+console.log('El nombre es: ' + persona.nombre)
